b4e879d4dce9bb86287db9ae3c27314700e3489c
1zemzem <1zemliakova@gmail.com>
2021-09-18 17:05:48 +0300

    new commit

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { Carousel, Button } from 'react-bootstrap';
+
+export default function Slider() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          height="700px"
+          src="./images/rozoviegori.png"
+          alt="gory"
+        />
+        <Carousel.Caption>
+          <h2>Хатха йога с элементами Animal flow</h2>
+          <p>
+            Animal Gymnastics Yoga – современная методика развития телесного
+            функционала
+          </p>
+          <Button>Подробнее</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          height="700px"
+          src="images.rozoviegori.png"
+          alt="gory"
+        />
+        <Carousel.Caption>
+          <h2>Хатха йога с элементами Animal flow</h2>
+          <p>
+            Animal Gymnastics Yoga – современная методика развития телесного
+            функционала
+          </p>
+          <Button>Подробнее</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          height="700px"
+          src="images.rozoviegori.png"
+          alt="gory"
+        />
+        <Carousel.Caption>
+          <h2>Хатха йога с элементами Animal flow</h2>
+          <p>
+            Animal Gymnastics Yoga – современная методика развития телесного
+            функционала
+          </p>
+          <Button>Подробнее</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
