96ec414d3ca38ffbb111fdf50d020978ccc1a7cd
1zemzem <1zemliakova@gmail.com>
2021-09-18 14:23:09 +0300

    navibar-styles

--- src/components/Navibar/Navibar.jsx
+++ src/components/Navibar/Navibar.jsx
@@ -1,47 +1,61 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
-import { Container, Nav, Navbar, Button } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .navbar-brand,
+  .nav-link {
+    color: #000000;
+    outline: none;
+  }
+`;
 
 export default function Navibar() {
   return (
     <div>
-      <Navbar collapseOnSelect expand="sm" bg="light" variant="light">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/">
-              <img
-                src="images/logo.svg"
-                width="130"
-                height="57"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-              ></img>
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link>
-                <Link to="/about">О нас</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/teachers">Преподаватели</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/timetable">Расписание</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/contacts">Контакты</Link>
-              </Nav.Link>
-            </Nav>
-            <Nav>
-              <Button variant="light">
-                <Link to="/login">login</Link>
-              </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Styles>
+        <Navbar collapseOnSelect expand="sm" bg="light" variant="light">
+          <Container>
+            <Navbar.Brand>
+              <Link to="/">
+                <img
+                  src="images/logo.svg"
+                  width="130"
+                  height="57"
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                ></img>
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Container>
+                <Nav className="me-auto">
+                  <Nav.Link>
+                    <Link to="/about">О нас</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to="/teachers">Преподаватели</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to="/timetable">Расписание</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to="/contacts">Контакты</Link>
+                  </Nav.Link>
+                </Nav>
+              </Container>
+              <Nav>
+                <Nav.Link>
+                  <Link to="/login">login</Link>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Styles>
     </div>
   );
 }
