28ecc57713d1ae890ee297e03432d98bdc7d9016
1zemzem <1zemliakova@gmail.com>
2021-09-21 17:13:50 +0300

    homepage2

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -12,51 +12,75 @@ export default function HomePage() {
         <h1>Преподаватели</h1>
       </Container>
       <Container fluid className="background-gray">
-        <Row>
-          <Col>
-            <Card style={{ width: '18rem', hight: '36rem' }}>
-              <Card.Img variant="top" src="/images/teacher1.png" />
-              <Card.Body>
-                <Card.Title>Инна Рыбчинская</Card.Title>
-                <Card.Text>Йога для начинающих</Card.Text>
-                <Button variant="outline-light">Записаться</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: '18rem', hight: '36rem' }}>
-              <Card.Img variant="top" src="/images/teacher2.png" />
-              <Card.Body>
-                <Card.Title>Василина Солтан</Card.Title>
-                <Card.Text>
-                  Сукшма Вьяяма - мощная, простая практика в традиции Дхирендры
-                  Брахмачари.
-                </Card.Text>
-                <Button variant="outline-light">Записаться</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: '18rem', hight: '36rem' }}>
-              <Card.Img variant="top" src="/images/teacher3.png" />
-              <Card.Body>
-                <Card.Title>Дарья Табольская</Card.Title>
-                <Card.Text>
-                  Йогатерапия - для тех, кто имеет ограничения здоровья.
-                </Card.Text>
-                <Button variant="outline-light">Записаться</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col sm={4}>
+              <Card>
+                <Card.Img variant="top" src="/images/teacher1.png" />
+                <Card.Body>
+                  <Card.Title>Инна Рыбчинская</Card.Title>
+                  <Card.Text>Йога для начинающих</Card.Text>
+                  <Button variant="outline-light">Записаться</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col sm={4}>
+              <Card>
+                <Card.Img variant="top" src="/images/teacher2.png" />
+                <Card.Body>
+                  <Card.Title>Василина Солтан</Card.Title>
+                  <Card.Text>
+                    Сукшма Вьяяма - мощная, простая практика в традиции
+                    Дхирендры Брахмачари.
+                  </Card.Text>
+                  <Button variant="outline-light">Записаться</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col sm={4}>
+              <Card>
+                <Card.Img variant="top" src="/images/teacher3.png" />
+                <Card.Body>
+                  <Card.Title>Дарья Табольская</Card.Title>
+                  <Card.Text>
+                    Йогатерапия - для тех, кто имеет ограничения здоровья.
+                  </Card.Text>
+                  <Button variant="outline-light">Записаться</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </Container>
-      <Container fluid className="px-0">
-        <img
-          className="d-block w-100"
-          height="900px"
-          src="images/back2.png"
-          alt=""
-        />
+      <Container
+        fluid
+        style={{
+          marginTop: '70px',
+          backgroundImage: 'url(' + 'images/back2.png' + ')',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos ratione
+          debitis quisquam temporibus maiores blanditiis hic sit, aspernatur
+          fuga doloribus similique, vero ea adipisci, dolorem ipsam possimus
+          magnam ipsa eum? Necessitatibus ut neque dolores atque, reprehenderit
+          eligendi odio tempore natus? Consequatur corporis minus deserunt quod,
+          eligendi in illum ipsam deleniti modi a facere ratione quibusdam?
+          Alias aperiam dolor quo laboriosam. Odit non provident, numquam
+          accusantium vitae voluptates incidunt quae. Corrupti placeat iste at
+          ab consequuntur culpa. Magnam sunt, veritatis deleniti assumenda
+          tempora iure similique nam sapiente! Porro incidunt sed accusamus.
+          Quam officia accusantium, debitis eius quidem saepe hic quaerat
+          eligendi. Culpa debitis totam facilis animi veniam eius voluptates
+          sapiente neque quos natus, molestias ipsum ab, quas saepe illo dicta
+          reprehenderit! Eligendi modi, soluta nemo dolorum hic recusandae
+          provident, illum ut, quis non cumque. Doloremque consectetur vero
+          similique quidem iure ipsa, dignissimos neque, quis nisi unde sequi
+          amet molestias deserunt eius.
+        </p>
       </Container>
     </>
   );
